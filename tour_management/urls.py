a08a8f7f368c3a367f034351a8183b07d455f88e
abir2776 <osmangoni255@gmail.com>
2026-01-06 13:25:59 +0600

    completed base setup

---
+++ tour_management/urls.py
@@ -0,0 +1,56 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tour_management API",
+        default_version="main",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    # Swagger
+    re_path(
+        r"^docs/swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=10),
+        name="schema-json",
+    ),
+    re_path(
+        r"^docs/swagger$",
+        schema_view.with_ui("swagger", cache_timeout=10),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^docs/redoc$",
+        schema_view.with_ui("redoc", cache_timeout=10),
+        name="schema-redoc",
+    ),
+    # JWT Token
+    path(
+        "api/v1/token",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "api/v1/token/refresh",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+    path(
+        "api/v1/token/verify",
+        TokenVerifyView.as_view(),
+        name="token_verify",
+    ),
+    path("admin/", admin.site.urls),
+]
