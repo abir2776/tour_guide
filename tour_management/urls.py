ab9caf2d812e79d9173be9938fde4c29b16a3d35
abir2776 <osmangoni255@gmail.com>
2026-01-16 14:19:38 +0600

    fixed image not showing issue

--- tour_management/urls.py
+++ tour_management/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
@@ -56,3 +58,4 @@ urlpatterns = [
     path("api/v1/auth/", include("core.rest.urls")),
     path("api/v1/tour/", include("tour_plan.rest.urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
