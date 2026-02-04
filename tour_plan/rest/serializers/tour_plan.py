c3f2007f2ed2dd3cd30b64abeb9fb195475599ae
abir2776 <osmangoni255@gmail.com>
2026-02-04 18:26:27 +0600

    added locations ids in tour plan

--- tour_plan/rest/serializers/tour_plan.py
+++ tour_plan/rest/serializers/tour_plan.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from tour_plan.models import Image, TourPlan
+from tour_plan.models import Image, Location, TourPlan
 from tour_plan.rest.serializers.image import ImageSerializer
 from tour_plan.rest.serializers.location import LocationSerializer
 
@@ -10,6 +10,9 @@ class TourPlanSerializer(serializers.ModelSerializer):
     images = ImageSerializer(many=True, read_only=True)
     image_ids = serializers.PrimaryKeyRelatedField(
         queryset=Image.objects.all(), many=True, write_only=True, source="images"
+    )
+    location_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Location.objects.all(), many=True, write_only=True, source="images"
     )
 
     class Meta:
