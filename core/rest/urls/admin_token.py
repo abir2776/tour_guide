aa855eaa676970cfc52b665d68dfc8e969904d13
abir2776 <osmangoni255@gmail.com>
2026-02-04 12:10:06 +0600

    fixed an name issue

--- core/rest/urls/admin_token.py
+++ core/rest/urls/admin_token.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from core.rest.views.admin_token import AdminTokenView
 
-urlpattens = [path("", AdminTokenView.as_view(), name="admin-token-create")]
+urlpatterns = [path("", AdminTokenView.as_view(), name="admin-token-create")]
