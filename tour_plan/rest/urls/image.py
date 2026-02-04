9e0d6085dd1db5a4eab316a41444ab8579b9f371
abir2776 <osmangoni255@gmail.com>
2026-02-04 16:52:52 +0600

    fixed image not delete issue

--- tour_plan/rest/urls/image.py
+++ tour_plan/rest/urls/image.py
@@ -4,5 +4,5 @@ from tour_plan.rest.views.image import ImageDetailsView, ImageListCreateView
 
 urlpatterns = [
     path("", ImageListCreateView.as_view(), name="image-list-create"),
-    path("<int:pk>", ImageDetailsView.as_view(), name="image-details"),
+    path("<int:id>", ImageDetailsView.as_view(), name="image-details"),
 ]
