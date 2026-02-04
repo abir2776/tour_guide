ace986d539e33162a99cf7ebd989e85cf9d379aa
abir2776 <osmangoni255@gmail.com>
2026-02-04 17:15:23 +0600

    added location details api

--- tour_plan/rest/urls/location.py
+++ tour_plan/rest/urls/location.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from tour_plan.rest.views.location import LocationListCreateAPIView
+from tour_plan.rest.views.location import LocationDetailsView, LocationListCreateAPIView
 
 urlpatterns = [
-    path("", LocationListCreateAPIView.as_view(), name="location-list-create")
+    path("", LocationListCreateAPIView.as_view(), name="location-list-create"),
+    path("<int:id>", LocationDetailsView.as_view(), name="location-details"),
 ]
--- tour_plan/rest/views/location.py
+++ tour_plan/rest/views/location.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 from tour_plan.models import Location
 from tour_plan.rest.serializers.location import LocationSerializer
@@ -7,3 +7,9 @@ from tour_plan.rest.serializers.location import LocationSerializer
 class LocationListCreateAPIView(ListCreateAPIView):
     serializer_class = LocationSerializer
     queryset = Location.objects.filter()
+
+
+class LocationDetailsView(RetrieveUpdateDestroyAPIView):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.filter()
+    lookup_field = "id"
