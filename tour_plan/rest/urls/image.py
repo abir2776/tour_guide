cbb22ae6ac0904e0d81eafa95e03fa0e47ab1d8a
abir2776 <osmangoni255@gmail.com>
2026-02-04 15:50:21 +0600

    added image details endpoint

--- tour_plan/rest/urls/image.py
+++ tour_plan/rest/urls/image.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from tour_plan.rest.views.image import ImageListCreateView
+from tour_plan.rest.views.image import ImageDetailsView, ImageListCreateView
 
-urlpatterns = [path("", ImageListCreateView.as_view(), name="image-list-create")]
+urlpatterns = [
+    path("", ImageListCreateView.as_view(), name="image-list-create"),
+    path("", ImageDetailsView.as_view(), name="image-details"),
+]
--- tour_plan/rest/views/image.py
+++ tour_plan/rest/views/image.py
@@ -1,9 +1,14 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 from tour_plan.models import Image
 from tour_plan.rest.serializers.image import ImageSerializer
 
 
 class ImageListCreateView(ListCreateAPIView):
+    serializer_class = ImageSerializer
+    queryset = Image.objects.filter()
+
+
+class ImageDetailsView(RetrieveUpdateDestroyAPIView):
     serializer_class = ImageSerializer
     queryset = Image.objects.filter()
