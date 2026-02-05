e092b9474a3d55376571825ef6f66dbda073e52a
abir2776 <osmangoni255@gmail.com>
2026-02-05 12:12:12 +0600

    add validation in date

--- tour_plan/rest/serializers/tour_date.py
+++ tour_plan/rest/serializers/tour_date.py
@@ -8,3 +8,17 @@ class TourDateSerializer(serializers.ModelSerializer):
         model = TourDate
         fields = "__all__"
         read_only_fields = ("tour_plan",)
+
+    def validate(self, attrs):
+        request = self.context.get("request")
+        view = self.context.get("view")
+
+        tour_id = view.kwargs.get("tour_id")
+        date = attrs.get("date")
+
+        if TourDate.objects.filter(tour_plan_id=tour_id, date=date).exists():
+            raise serializers.ValidationError(
+                {"date": "This date already exists for this tour."}
+            )
+
+        return attrs
