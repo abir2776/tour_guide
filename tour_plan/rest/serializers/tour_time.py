6439dd24ff6e3faea34edf716c7bfa06d6b8b85e
abir2776 <osmangoni255@gmail.com>
2026-02-05 15:02:50 +0600

    added tour date in time slot

--- tour_plan/rest/serializers/tour_time.py
+++ tour_plan/rest/serializers/tour_time.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
+
 from tour_plan.models import TimeSlot
+from tour_plan.rest.serializers.tour_date import TourDateSerializer
 
 
 class TimeSlotSerializer(serializers.ModelSerializer):
+    tour_date = TourDateSerializer(read_only=True)
+
     class Meta:
         model = TimeSlot
         fields = "__all__"
-        read_only_fields = ("tour_date","end_time")
+        read_only_fields = ("tour_date", "end_time")
 
     def validate(self, attrs):
         view = self.context.get("view")
