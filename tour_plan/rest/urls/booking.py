dffe0ec47abbffa33bc8f974bd1a2269006c3854
abir2776 <osmangoni255@gmail.com>
2026-02-09 11:31:49 +0600

    fixed showing booking details get issue

--- tour_plan/rest/urls/booking.py
+++ tour_plan/rest/urls/booking.py
@@ -10,8 +10,8 @@ from tour_plan.rest.views.booking import (
 urlpatterns = [
     path("", BookingListCreateAPIView.as_view(), name="booking-list-create"),
     path(
-        "<int:pk>", BookingRetrieveUpdateDestroyAPIView.as_view(), name="booking-detail"
+        "<int:id>", BookingRetrieveUpdateDestroyAPIView.as_view(), name="booking-detail"
     ),
     path("items", BookingItemListCreate.as_view(), name="booking-item-list-create"),
-    path("items/<int:pk>", BookingItemDetails.as_view(), name="booking-item-details"),
+    path("items/<int:id>", BookingItemDetails.as_view(), name="booking-item-details"),
 ]
